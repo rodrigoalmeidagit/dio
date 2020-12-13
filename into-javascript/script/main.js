0b6da4dee3c6083c4e9ab14375226cd1e6b753d4
rodrigoalmeidagit <novators@gmail.com>
2020-12-13 02:24:08 -0300

    updated index.html e main.js

--- into-javascript/script/main.js
+++ into-javascript/script/main.js
@@ -1,5 +1,23 @@
+function clicou() {
+    document.getElementById('agradecimento').innerHTML = '<b>Obrigado por clicar!</b>'    
+}
 
-var nome = 'Rodrigo Almeida';
-var idade = 29;
-var idade2 = 10;
-//alert(`${nome} tem ${idade}!`)
+/*function redirecionar() {
+    window.location.href = 'https://globo.com'
+}
+*/
+function trocar(element){
+    element.innerHTML = 'Obrigado por passar o mouse!'
+}
+
+function voltar(element){
+    element.innerHTML = 'Passe o mouse aqui!'
+}
+
+function load(){
+    alert('Pagina Carregada')
+}
+
+function funcaoChance(elemento){
+    console.log(elemento.value)
+}
